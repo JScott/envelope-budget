a1788af45bac1c57736ec41e6b1445f1d271b539
JScott <jvscott@gmail.com>
2015-01-26 14:31:42 -0800

    Same thing but in KnockoutJS

--- budget.js
+++ budget.js
@@ -1,36 +1,34 @@
-var budgetApp = angular.module('budgetApp', []);
+// Will have some trouble with older browsers
+document.addEventListener('DOMContentLoaded', function() {
 
-budgetApp.controller('EnvelopeCtrl', function($scope) {
-  $scope.envelopes = [{
-    name: 'test',
-    income: 50.01,
-    start: new Date()
-  },
-  {
-    name: 'test2',
-    income: 1,
-    start: new Date()
-  }]
-  
-  $scope.save = function() {
-    $scope.envelopes.push($scope.newEnvelope);
-    $scope.resetForm();
-  };
-  
-  $scope.resetForm = function() {
-    $scope.newEnvelope = {
-      name: 'name',
-      income: 0,
-      start: new Date()
-    };
+  function Envelope(data) {
+    this.name = ko.observable(data.name);
+    this.income = ko.observable(data.income);
+    this.startDate = ko.observable(data.startDate)
   }
   
-  $scope.resetForm();
+  function EnvelopeListViewModel() {
+    var self = this;
+    self.envelopes = ko.observableArray([
+      new Envelope({name: 'test1', income: 10, startDate: new Date()}),
+      new Envelope({name: 'test2', income: 50, startDate: new Date()})
+    ]);
+    self.newEnvelope = ko.observable({});
+  
+    self.resetForm = function() {
+      self.newEnvelope = {};
+    }
+  
+    self.addEnvelope = function() {
+        console.log(this.newEnvelope);
+      var newEnvelope = new Envelope(this.newEnvelope);
+        console.log(newEnvelope);
+        console.log(newEnvelope.name());
+      self.envelopes.push(newEnvelope);
+        console.log(self.envelopes);
+      self.resetForm();
+    };
+  }
+      
+  ko.applyBindings(new EnvelopeListViewModel());
 });
-
-
-
-// compare - new Date(localStorage['last_visit']). be sure to check if undefined
-localStorage['last_visit'] = new Date();
-
-//envelopes = localStorage['envelopes'] || []
--- test/budgetSpec.js
+++
@@ -1,6 +0,0 @@
-describe('EnvelopeCtrl', function() {
-  beforeEach(module('budgetApp'));
-  //scope = {}
-  //ctrl = $controller('EnvelopeCtrl', {$scope:scope})
-  //expect(scope.phones.length).toBe(3);
-});
